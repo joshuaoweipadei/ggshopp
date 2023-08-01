cd168c13a1786de2d7007b68e395dc18819f1c55
Nikasha12 <141149619+Nikasha12@users.noreply.github.com>
2023-08-01 11:06:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,66 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <title>GGSHOP</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f7f7;
+            color: #333;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #ADD8E6;
+            color: #fff;
+            padding: 10px;
+            text-align: center;
+        }
+
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        .description {
+            font-size: 16px;
+            margin-bottom: 20px;
+        }
+
+        .button {
+            background-color: #ADD8E6;
+            border: none;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 10px 0;
+            cursor: pointer;
+            border-radius: 4px;
+        }
+
+        .button:hover {
+            background-color: #00008B;
+        }
+    </style>
 </head>
 <body>
-  <div>Group G</div>
+    <header>
+        <h1>Welcome to GGSHOP Style</h1>
+    </header>
+
+    <div class="container">
+        <div class="description">
+            <h2>Who We Are</h2>
+            <p>Discover your style with our chic and user-friendly ecommerce website for clothes. Browse a diverse collection of fashion-forward apparel for men, women, and kids. Enjoy high-quality imagery, detailed product descriptions, and easy size guides for a seamless shopping experience. With secure checkout, swift delivery, and excellent customer support, revamp your wardrobe hassle-free. Don't miss exclusive offers and discounts to elevate your fashion game. Shop now and embrace your unique style effortlessly!</p>
+        </div>
+
+        <a href="#" class="button">Shop Now</a>
+    </div>
 </body>
 </html>
