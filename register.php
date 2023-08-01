b0f8678a22ea22b3717068249d37e0e431d1dd95
joshuaoweipadei <oweipadeijoshie@gmail.com>
2023-08-01 09:45:14 -0400

    hi

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Regsiter</title>
+</head>
+<body>
+  <div>
+    <form action="" method="post">
+      <div>
+        <input type="text" name="fullname" class="input_field" placeholder="Full name" >
+      </div>
+      <div>
+        <input type="text" name="email" class="input_field" placeholder="Enter email" >
+      </div>
+      <div>
+        <input type="text" name="password" class="input_field" placeholder="Enter Password" >
+      </div>
+      <div>
+        <button type="submit" class="submit_btn">Register Now</button>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
